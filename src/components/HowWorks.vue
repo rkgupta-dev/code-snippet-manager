4e3d8289d90886436b0a0f76277626347f3572b3
rkgupta-dev <rohitkrgupta333@gmail.com>
2024-12-12 22:03:10 +0530

    updates HowWorks.vue page.

--- src/components/HowWorks.vue
+++ src/components/HowWorks.vue
@@ -2,7 +2,7 @@
   <div>
     <v-container fluid class="my-2">
       <div class="text-center" color="primary" dark>
-        <span class="display-1 font-weight-bold">How QuickSnip Works</span>
+        <h2 class="font-weight-bold">How QuickSnip Works</h2>
       </div>
     </v-container>
     <v-divider></v-divider>
@@ -12,12 +12,12 @@
           <!-- Step 1: Save Snippets -->
           <v-row class="mt-5" align="center">
             <v-col cols="12" md="4">
-              <v-icon color="primary" class="display-4"
+              <v-icon color="primary" class="display-3"
                 >mdi-file-document</v-icon
               >
             </v-col>
             <v-col cols="12" md="8">
-              <h3 class="text-h5 font-weight-bold">
+              <h3 class="text-h6 font-weight-bold">
                 Step 1: Save Your Snippets
               </h3>
               <p>
@@ -31,12 +31,12 @@
           <!-- Step 2: Copy Snippets -->
           <v-row class="mt-5" align="center">
             <v-col cols="12" md="4">
-              <v-icon color="primary" class="display-4"
+              <v-icon color="primary" class="display-3"
                 >mdi-content-copy</v-icon
               >
             </v-col>
             <v-col cols="12" md="8">
-              <h3 class="text-h5 font-weight-bold">Step 2: Copy Snippets</h3>
+              <h3 class="text-h6 font-weight-bold">Step 2: Copy Snippets</h3>
               <p>
                 Once your snippet is saved, you can easily copy it to your
                 clipboard with the click of a button. Just hit the "Copy"
@@ -48,10 +48,10 @@
           <!-- Step 3: Search and Filter Snippets -->
           <v-row class="mt-5" align="center">
             <v-col cols="12" md="4">
-              <v-icon color="primary" class="display-4">mdi-magnify</v-icon>
+              <v-icon color="primary" class="display-3">mdi-magnify</v-icon>
             </v-col>
             <v-col cols="12" md="8">
-              <h3 class="text-h5 font-weight-bold">
+              <h3 class="text-h6 font-weight-bold">
                 Step 3: Search and Filter Snippets
               </h3>
               <p>
@@ -65,10 +65,10 @@
           <!-- Step 4: Share Snippets -->
           <v-row class="mt-5" align="center">
             <v-col cols="12" md="4">
-              <v-icon color="primary" class="display-4">mdi-whatsapp</v-icon>
+              <v-icon color="primary" class="display-3">mdi-whatsapp</v-icon>
             </v-col>
             <v-col cols="12" md="8">
-              <h3 class="text-h5 font-weight-bold">Step 4: Share Snippets</h3>
+              <h3 class="text-h6 font-weight-bold">Step 4: Share Snippets</h3>
               <p>
                 Share your favorite code snippets with others easily via
                 WhatsApp. Just click the "Share" button and choose your desired
